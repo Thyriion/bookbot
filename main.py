b92d96161b7975d3fa817470e100e38c972983f8
Thyriion <kevinschueler2000@gmail.com>
2025-05-12 08:20:12 +0200

    refactor in multiple files

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import count_words
+from stats import count_characters_in_text
+
 def get_book_text(path) -> str:
     """
     Reads the content of a book file and returns it as a string.
@@ -18,6 +21,9 @@ def main():
     """
     book_text = get_book_text('./books/frankenstein.txt')
 
-    print(book_text)
+    num_words = count_words(book_text)
+    characters_in_text = count_characters_in_text(book_text)
+    print(f"{num_words} words found in the document.")
+    print(characters_in_text)
 
 main()
